7ceb3a2a6a2f04f1ba1b7d309cb5c5a7ff87f381
DESKTOP-F08TJT5\data <mailtogeck@gmail.com>
2021-03-18 17:07:01 +0100

    icons + hyperlink handling

--- src/extension/services/commands.ts
+++ src/extension/services/commands.ts
@@ -1,10 +1,10 @@
 import * as vscode from 'vscode';
+import { Location } from '../../shared/models';
 
-export const jumpToLineHandler = (uri: string, startLine: number, startColumn: number, endLine: number, endColumn: number) => {
-	const startPosition = new vscode.Position(startLine, 0);
-	const endPosition = new vscode.Position(endLine, endColumn);
-
-	vscode.commands.executeCommand("vscode.open", vscode.Uri.parse("odx:"+uri), {  selection: new vscode.Range(startPosition, endPosition) ,preserveFocus:true});
+export const jumpToLineHandler = (location: Location, preview: boolean) => {
+	const startPosition = new vscode.Position(location.startLine, 10);
+	const endPosition = new vscode.Position(location.endLine, location.endColumn);
+	vscode.commands.executeCommand("vscode.open", vscode.Uri.parse("odx:" + location.fileUri), { selection: new vscode.Range(startPosition, endPosition), preserveFocus: true, preview: preview });
 };
 
 export const odxSourceHandler = async (odxSource: vscode.Uri) => {
